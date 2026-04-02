8bd9c9071bfcbce78792d86cbc4cefe9f022fb68
Kingxtrem <tantubaykingshuk@gmail.com>
2026-04-02 10:29:46 +0530

    fix

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,14 @@
 
 export const authApi = {
   login: (payload) => axiosInstance.post("/auth/login", payload),
+
   refresh: () => axiosInstance.post("/auth/refreshaccessToken"),
+
   logout: () => axiosInstance.post("/auth/logout"),
+
   profile: () => axiosInstance.get("/settings/getProfile")
+  
 };
+
+
+
